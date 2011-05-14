a88ee0adb72d019cf263a7efd455271413d3a324
Brian Kaney <brian@vermonster.com>
2011-05-14 18:51:30 -0400

    Refactoring, made the b64encode more pure.

--- lib/oplop.rb
+++ lib/oplop.rb
@@ -10,31 +10,33 @@ module Oplop
   #
   def self.password(args={})
     unless (args.keys.include?(:master) && args.keys.include?(:label))
-      raise ArgumentError.new "Master and label are required arguments."
+      raise ArgumentError.new 'Master and label are required arguments.'
     end
     
-    master_label = "%s%s" % [ args.delete(:master), args.delete(:label) ]
+    master_label = '%s%s' % [ args.delete(:master), args.delete(:label) ]
     
-    raise ArgumentError.new "Unknown keys #{args.keys.join(",")}." if args.keys.any?
+    raise ArgumentError.new "Unknown keys #{args.keys.join(',')}." if args.keys.any?
     
-    password = urlsafe_b64encode(master_label)
+
+    password = urlsafe_b64encode(Digest::MD5.digest(master_label))
     
     if password.respond_to?(:encode)
       password = password.encode('UTF-8')
     end
     
     if m = password.match(/\d+/)
-      password = "%s%s" % [ m[0], password ] if (m.begin(0) >= LENGTH)
+      password = '%s%s' % [ m[0], password ] if (m.begin(0) >= LENGTH)
     else
-      password = "1%s" % password
+      password = '1%s' % password
     end
     
     password[0,LENGTH]
   end
 
   # See http://www.ietf.org/rfc/rfc4648.txt
+  # Ruby 1.8.x does not have this as part of the Base64 lib
   def self.urlsafe_b64encode(string)
-    Base64.encode64(Digest::MD5.digest(string)).tr('+/', '-_').gsub("\n",'')
+    Base64.encode64(string).tr('+/', '-_')
   end
 
 end
