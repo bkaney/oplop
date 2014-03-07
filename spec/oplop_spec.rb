473f820a2a744679df7e06c722d94f6494c72827
Brian Kaney <brian@vermonster.com>
2014-03-06 20:09:05 -0500

    Rewrite specs to use new expects form

--- spec/oplop_spec.rb
+++ spec/oplop_spec.rb
@@ -1,34 +1,27 @@
 require 'spec_helper'
 
 describe Oplop do
-  
   describe ".password" do
-
     specify "valid arguments" do
-      lambda{ Oplop.password(:master => "a", :label => "a") }.should_not raise_error(ArgumentError)
+      expect { Oplop.password(:master => "a", :label => "a") }.not_to raise_error
     end
 
     specify "invalid arguments" do
-      lambda{ Oplop.password }.should raise_error(ArgumentError, /Master and label are required/)
+      expect { Oplop.password }.to raise_error(ArgumentError, /Master and label are required/)
     end
 
     specify "invalid arguments" do
-      lambda{ Oplop.password(:master => "a", :label => "b", :foo => 'c') }.should raise_error(ArgumentError, /Unknown key/)
+      expect { Oplop.password(:master => "a", :label => "b", :foo => 'c') }.to raise_error(ArgumentError, /Unknown key/)
     end
 
     context "tests from testdata.json (see Python implementation at http://code.google.com/p/oplop/)" do
-
       # loop around each "case" in testdata.yml
       Yajl::Parser.new.parse(File.new(File.dirname(__FILE__) + "/testdata.json", 'r')).each do |testdata|
-
         specify testdata["why"] do
-          Oplop.password(:master => testdata["master"], :label => testdata["label"]).should == testdata["password"]
+          password = Oplop.password(:master => testdata["master"], :label => testdata["label"])
+          expect(password).to eq testdata["password"]
         end
-
       end
-
     end
-
   end
-
 end
