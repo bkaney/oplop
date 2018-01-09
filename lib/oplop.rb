516173ac689248f79589c5a5c33d75949dd79304
Brian Kaney <brian@vermonster.com>
2018-01-09 15:44:15 -0500

    hack to start supporting loplop

--- lib/oplop.rb
+++ lib/oplop.rb
@@ -3,7 +3,8 @@ require 'base64'
 
 module Oplop
 
-  LENGTH = 8
+  CLASSIC_LENGTH = 8
+  DEFAULT_LENGTH = 16
 
   #
   # Oplop.password(:master => 'p@ssw0rd', :label => 'github')
@@ -12,25 +13,36 @@ module Oplop
     unless (args.keys.include?(:master) && args.keys.include?(:label))
       raise ArgumentError.new 'Master and label are required arguments.'
     end
-    
-    master_label = '%s%s' % [ args.delete(:master), args.delete(:label) ]
-    
+
+    length = DEFAULT_LENGTH
+    label = args.delete(:label)
+    master = args.delete(:master)
+
+    if label =~ /^([0-9]*)?\*/
+      (length, label) = label.split('*')
+      length = length.to_i
+      length = CLASSIC_LENGTH if length <= 0
+      length = 22 if length > 22
+    end
+
+    master_label = '%s%s' % [ master, label ]
+
     raise ArgumentError.new "Unknown keys #{args.keys.join(',')}." if args.keys.any?
-    
+
 
     password = urlsafe_b64encode(Digest::MD5.digest(master_label))
-    
+
     if password.respond_to?(:encode)
       password = password.encode('UTF-8')
     end
-    
+
     if m = password.match(/\d+/)
-      password = '%s%s' % [ m[0], password ] if (m.begin(0) >= LENGTH)
+      password = '%s%s' % [ m[0], password ] if (m.begin(0) >= length)
     else
       password = '1%s' % password
     end
-    
-    password[0,LENGTH]
+
+    password[0,length]
   end
 
   # See http://www.ietf.org/rfc/rfc4648.txt
--- spec/oplop_spec.rb
+++ spec/oplop_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Oplop do
   describe ".password" do
     specify "valid arguments" do
-      expect { Oplop.password(:master => "a", :label => "a") }.not_to raise_error
+      expect { Oplop.password(:master => "a", :label => "*a") }.not_to raise_error
     end
 
     specify "invalid arguments" do
@@ -11,14 +11,14 @@ describe Oplop do
     end
 
     specify "invalid arguments" do
-      expect { Oplop.password(:master => "a", :label => "b", :foo => 'c') }.to raise_error(ArgumentError, /Unknown key/)
+      expect { Oplop.password(:master => "a", :label => "*b", :foo => 'c') }.to raise_error(ArgumentError, /Unknown key/)
     end
 
     context "tests from testdata.json (see Python implementation at http://code.google.com/p/oplop/)" do
       # loop around each "case" in testdata.yml
       Yajl::Parser.new.parse(File.new(File.dirname(__FILE__) + "/testdata.json", 'r')).each do |testdata|
         specify testdata["why"] do
-          password = Oplop.password(:master => testdata["master"], :label => testdata["label"])
+          password = Oplop.password(:master => testdata["master"], :label => "*" + testdata["label"])
           expect(password).to eq testdata["password"]
         end
       end
